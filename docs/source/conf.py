3c7f49ee4366f505b7da1ef60dffa88382be7d4c
jespervangilsgain <99016852+jespervangilsgain@users.noreply.github.com>
2022-02-16 12:51:24 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GBL'
+copyright = '2021, Gain'
+author = 'Gain'
 
 release = '0.1'
 version = '0.1.0'
